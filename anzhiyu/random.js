3f32bccd043ae670bd225be1e0db82061b1e70b2
YuleBest <yule-best@outlook.com>
2024-09-29 12:03:49 +0800

    Site updated: 2024-09-29 12:03:48

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["post3/","post9/","fadian/","post10/","post7/","post8/","post6/","post11/","post12/","post2/","post5/","post4/","post1/","quote/"];function toRandomPost(){
+var posts=["post3/","post9/","fadian/","post10/","post7/","post8/","post6/","quote/","post11/","post12/","post2/","post5/","post4/","post1/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };

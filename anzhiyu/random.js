f853d28a81cb146b59ccbc2df3663b4ade648cfe
YuleBest <yule-best@outlook.com>
2024-10-28 12:32:47 +0800

    Site updated: 2024-10-28 12:32:46

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["post3/","post14/","app/","post9/","fadian/","post10/","film/001/","post7/","post8/","post6/","quote/","post11/","post12/","post2/","post5/","post4/","post1/"];function toRandomPost(){
+var posts=["post14/","post3/","app/","post9/","fadian/","post10/","film/001/","post7/","post8/","post6/","pinglun001/","quote/","post11/","post12/","post2/","post5/","post4/","post1/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };

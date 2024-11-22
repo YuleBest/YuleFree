cd499651e08ff64d5136997bc3dc29ad28f98b39
YuleBest <yule-best@outlook.com>
2024-11-22 22:43:20 +0800

    Site updated: 2024-11-22 22:43:19

--- anzhiyu/random.js
+++
@@ -1,3 +0,0 @@
-var posts=["post14/","post3/","post15/","app/","post9/","lsp/","fadian/","post10/","film/001/","post7/","post8/","post6/","pinglun001/","quote/","post11/","post12/","post2/","post5/","post1/","post4/"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
